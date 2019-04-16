702010c4bb91efc752f6d3912f13b672cc5b7370
BigBricks <bsa6.23.94@gmail.com>
2019-04-16 16:38:33 -0400

    Working on interconnected relations team v player

--- FunWithDjango/baseball/models.py
+++ FunWithDjango/baseball/models.py
@@ -3,14 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Team(models.Model):
     team_name = models.CharField(max_length=100)
+    players = models.ManyToManyField(Player)
 
     def __str__(self):
-        return self.team_name
+        return "%s %s" % (self.team_name, self.players)
+
 
 class Player(models.Model):
     player_name = models.CharField(max_length=100)
-    team = models.CharField(max_length=100)
+    team = models.ForeignKey(Team)
     position = models.CharField(max_length=2)
+
+
 # Create your models here.
