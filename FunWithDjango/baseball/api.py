97c0a7c1d9a866a410bc581bf10736d233110bc3
BigBricks <bsa6.23.94@gmail.com>
2019-04-16 13:43:55 -0400

    Serializer

--- FunWithDjango/baseball/api.py
+++ FunWithDjango/baseball/api.py
@@ -2,3 +2,8 @@ from baseball.models import Team, Player
 from rest_framework import viewsets, permissions
 from .serializers import TeamSerializer, PlayerSerializer
 
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = PlayerSerializer
