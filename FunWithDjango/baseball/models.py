d57395df09b65f46a584c4cc830f961637122a7e
BigBricks <bsa6.23.94@gmail.com>
2019-04-16 13:36:25 -0400

    Working on Player and Team models

--- FunWithDjango/baseball/models.py
+++ FunWithDjango/baseball/models.py
@@ -3,8 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Team(models.Model):
+    team_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.team_name
+
 class Player(models.Model):
-    name = models.CharField(max_length=100)
+    player_name = models.CharField(max_length=100)
     team = models.CharField(max_length=100)
     position = models.CharField(max_length=2)
 # Create your models here.
