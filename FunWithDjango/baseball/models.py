0d81962abd6b91420d8e5286ef4f792bb73c16c8
BigBricks <bsa6.23.94@gmail.com>
2019-04-16 13:32:44 -0400

    Beginning of player model.

--- FunWithDjango/baseball/models.py
+++ FunWithDjango/baseball/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    position = models.CharField(max_length=2)
 # Create your models here.
